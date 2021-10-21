7c8f7fe068d955026ed9d4aadcbcb8f3749f3825
Suyatna <zuyatna@gmail.com>
2021-10-21 11:36:02 +0700

    solve exercise 3.1

---
+++ Chapter03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chapter03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 8;
+            int denominator = 3;
+
+            try
+            {
+                int divideByZero = input / denominator;
+
+                System.Console.WriteLine(divideByZero);
+            }
+            catch (FormatException) 
+            {
+                System.Console.WriteLine("input and denominator is int");
+            }
+            catch (System.Exception)
+            {               
+                System.Console.WriteLine("denominator shouldn't value by zero!");
+            }
+        }
+    }
+}
