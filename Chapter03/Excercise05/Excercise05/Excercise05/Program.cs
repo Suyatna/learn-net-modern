08af09462ac9a1e13a750722ca7ca44416216e0d
Suyatna <zuyatna@gmail.com>
2021-10-22 16:26:54 +0700

    solve exercise05

---
+++ Chapter03/Excercise05/Excercise05/Excercise05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Excercise05
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // // 1.
+            int x = 3;
+            int y = 2 + ++x;
+            Console.WriteLine($"x: {x}, y: {y}");
+
+            Console.WriteLine();
+            
+            // 2.
+            x = 3 << 2;
+            y = 10 >> 1;
+            Console.WriteLine($"x: {x}, y: {y}");
+        }
+    }
+}
