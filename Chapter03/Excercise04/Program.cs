9ab8df65f38c38b8df75f52e2e3da3e8be662839
Suyatna <zuyatna@gmail.com>
2021-10-22 16:02:26 +0700

    solve excercise3.4

---
+++ Chapter03/Excercise04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Excercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter a number between 0 and 255: ");    
+            string inputNumber = Console.ReadLine();
+
+            System.Console.Write("Enter another number between 0 and 255: ");
+            string inputAnotherNumber = Console.ReadLine();
+
+            try
+            {
+                int value = int.Parse(inputNumber) / int.Parse(inputAnotherNumber);
+
+                System.Console.WriteLine($"{inputNumber} divided by {inputAnotherNumber} is {value}");
+            }
+            catch (FormatException)
+            {
+                System.Console.WriteLine("FormatException: Input string was not in a corrent format.");        
+            }
+        }
+    }
+}
