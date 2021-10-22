a91647fc38cc8cbe92f895ec625a1a6537a55d3b
Suyatna <zuyatna@gmail.com>
2021-10-22 13:50:05 +0700

    solve exercise3.3

---
+++ Chapter03/Excercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Excercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                WriteLine(i);
+
+                if (i == byte.MaxValue)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
